66138a6534c1e6f402bba6d35018375c286f7b8b
Murillo do Carmo <murillodocarmo@gmail.com>
2016-08-24 15:06:08 -0300

    Local config

--- routes/config.js
+++ routes/config.js
@@ -3,13 +3,13 @@ var config = {};
 config.settings = {};
 
 // sets the number of results shown on the home page
-config.settings.num_top_results = 10;
+config.settings.num_top_results = 20;
 
 // sets whether the view count will be shown next to the top results on the homepage
 config.settings.show_view_count = true;
 
 // sets the website title
-config.settings.website_title = "openKB";
+config.settings.website_title = "BM - Base de Conhecimento";
 
 // If set to "true", this locks down all pages of the blog and requires an authenticated user
 config.settings.password_protect = false;
